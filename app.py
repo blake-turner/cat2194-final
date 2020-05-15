64c7b8bc3e8f31400c64d8d83070e246de589231
Blake Turner <blaketurner@Blakes-MacBook-Air.local>
2020-05-15 13:31:14 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,20 @@
+#import statements
+from flask import Flask, render_template
+
+#Flask app variable
+app = Flask(__name__)
+
+#static route
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/classes")
+def classes():
+    return render_template("classes.html")
+
+#start the server
+if __name__ == "__main__":
+    app.run()
+    
+
